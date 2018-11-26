e77b40de54d801e5938fd952f11c8a34d3fb8e85
Mark Lockett <mark@deliveryengine.net>
2018-11-27 00:01:05 +0800

    Rewrite the runner to use the new Demux class

--- runner.py
+++ runner.py
@@ -1,4 +1,4 @@
-from demux import demux
+from demux import Demux
 
 # Callback start block function (OPTIONAL)
 # Starts this block and stores primitive details in django database
@@ -33,31 +33,29 @@ def effect_action(action, **kwargs):
 def commit_block(block):
     print ("Block " + str(block['block_num']) + " commited.")
 
-
-demux.initialise_action_dict()
+d = Demux(start_block_fn=start_block, commit_block_fn=commit_block)
 # Tells demux there are callback functions and registers them
-demux.register_start_commit(start_block, commit_block)
-demux.register_action(action, "eosio.unregd", "add")
-demux.register_action(action2, "eosio.unregd", "add")
-demux.register_action(action1)
-demux.register_action(action3, "randomAccount", "randomName")
-demux.register_action(effect_action, is_effect=True)
+d.register_action(action, "eosio.unregd", "add")
+d.register_action(action2, "eosio.unregd", "add")
+d.register_action(action1)
+d.register_action(action3, "randomAccount", "randomName")
+d.register_action(effect_action, is_effect=True)
 
 # Input block number to be stored
 block_num = input('Enter a block number: ')
 # Iterates through the transactions and actions of the block number
-demux.process_block(block_num)
-demux.process_block(block_num, include_effects=True)
-#print("action_dict=", demux.action_dict)
-print("head_block=", demux.head_block)
+d.process_block(block_num)
+d.process_block(block_num, include_effects=True)
+#print("action_dict=", d.action_dict)
+print("head_block=", d._head_block)
 
 
 # Input a start and end block for multi-block processing
-#start_block = int(input('Enter start block number: '))
-#end_block = int(input('Enter end block number: '))
+start_block = int(input('Enter start block number: '))
+end_block = int(input('Enter end block number: '))
 #end_block = None
 # Input a start and end block for multi-block processing
-#demux.process_blocks(start_block)
-#demux.process_blocks(start_block, end_block, include_effects=True) # only effects
-#demux.process_blocks(start_block, end_block) # only updates
+#d.process_blocks(start_block)
+#d.process_blocks(start_block, end_block, include_effects=True) # only effects
+#d.process_blocks(start_block, end_block) # only updates
 #print('action_dict=', demux.action_dict)
