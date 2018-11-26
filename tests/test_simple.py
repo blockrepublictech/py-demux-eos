0e39bb710b2fe5b32b4cc4f0d9b76f7976eabf65
Mark Lockett <mark@deliveryengine.net>
2018-11-26 20:15:49 +0800

    Mock out calls to the internet in simple tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -3,10 +3,11 @@ import pytest
 from unittest.mock import Mock, patch
 from eosapi import Client
 from demux.demux import register_start_commit, register_action, process_block, process_blocks, get_head_block, initialise_action_dict
+from tests.utils import fake_block1
 
 # Basic tests for pydemux, run the process_block() functions and connect to the Client
 
-@pytest.mark.skip(reason="Takes too long to execute, connects to Client each time")
+
 class TestSimplePyDemux(unittest.TestCase):
 
     def test_register_all_callback_functions(self):
@@ -20,6 +21,7 @@ class TestSimplePyDemux(unittest.TestCase):
         def commit():
             print('Block committed.')
 
+        initialise_action_dict()
         register_start_commit(start, commit)
         register_action(action)
         from demux.demux import start_block_fn, commit_block_fn
@@ -28,11 +30,14 @@ class TestSimplePyDemux(unittest.TestCase):
 #        assert action_fn == action
         assert commit_block_fn == commit
 
-#    @pytest.mark.skip(reason="Not sure why this test is failing, says action() gets unexpected kwarg block")
-    def test_start_and_commit_callback_functions_called(self):
+    @patch.object(Client, 'get_block')
+    @patch.object(Client, 'get_info')
+    def test_start_and_commit_callback_functions_called(self, mock_get_info_head_block, mock_get_block):
         """
         Ensure the start_block and commit_block functions are called when processing a block
         """
+        mock_get_info_head_block.return_value = {'head_block_num': 99999999999}
+        mock_get_block.return_value = fake_block1
         # set up the action_dict
         initialise_action_dict()
         # mock callback functions
@@ -48,10 +53,14 @@ class TestSimplePyDemux(unittest.TestCase):
         mock_commit_block.assert_called_once()
 
 
-    def test_no_start_block_function(self):
+    @patch.object(Client, 'get_block')
+    @patch.object(Client, 'get_info')
+    def test_no_start_block_function(self, mock_get_info_head_block, mock_get_block):
         """
         When users do not want to use the start_block function
         """
+        mock_get_info_head_block.return_value = {'head_block_num': 99999999999}
+        mock_get_block.return_value = fake_block1
         # set up the action_dict
         initialise_action_dict()
         # mock callback functions
@@ -59,7 +68,7 @@ class TestSimplePyDemux(unittest.TestCase):
         mock_action = Mock()
         mock_commit_block = Mock()
 
-        register_start_commit(mock_commit_block)
+        register_start_commit(commit_block=mock_commit_block)
         register_action(mock_action)
         process_block(9999)
 
@@ -67,10 +76,14 @@ class TestSimplePyDemux(unittest.TestCase):
         mock_action.assert_called()
         mock_commit_block.assert_called_once()
 
-    def test_no_commit_block_function(self):
+    @patch.object(Client, 'get_block')
+    @patch.object(Client, 'get_info')
+    def test_no_commit_block_function(self, mock_get_info_head_block, mock_get_block):
         """
         When users do not want to use the commit_block function
         """
+        mock_get_info_head_block.return_value = {'head_block_num': 99999999999}
+        mock_get_block.return_value = fake_block1
         # set up the action_dict
         initialise_action_dict()
         # mock callback functions
@@ -86,10 +99,14 @@ class TestSimplePyDemux(unittest.TestCase):
         mock_action.assert_called()
         mock_commit_block.assert_not_called()
 
-    def test_only_action_function(self):
+    @patch.object(Client, 'get_block')
+    @patch.object(Client, 'get_info')
+    def test_only_action_function(self, mock_get_info_head_block, mock_get_block):
         """
         When users only want to use the action function
         """
+        mock_get_info_head_block.return_value = {'head_block_num': 99999999999}
+        mock_get_block.return_value = fake_block1
         # set up the action_dict
         initialise_action_dict()
         # mock callback functions
@@ -105,10 +122,14 @@ class TestSimplePyDemux(unittest.TestCase):
         mock_commit_block.assert_not_called()
 
 
-    def test_multiple_block_processing_with_start_and_end_block(self):
+    @patch.object(Client, 'get_block')
+    @patch.object(Client, 'get_info')
+    def test_multiple_block_processing_with_start_and_end_block(self, mock_get_info_head_block, mock_get_block):
         """
         Ensure we can process multiple blocks with a start and end block
         """
+        mock_get_info_head_block.return_value = {'head_block_num': 99999999999}
+        mock_get_block.return_value = fake_block1
         # set up the action_dict
         initialise_action_dict()
         # mock callback functions
